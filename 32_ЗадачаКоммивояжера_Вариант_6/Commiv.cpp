258feedc1a6a6facd72c7415d83788a30f99718b
Ushakov Andrey РИС-19-1б <65588137+leenzstra@users.noreply.github.com>
2020-05-19 12:22:00 +0500

    Add files via upload

---
+++ 32_ЗадачаКоммивояжера_Вариант_6/Commiv.cpp
@@ -0,0 +1,20 @@
+﻿#include "graph.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "rus");
+	glutInit(&argc, argv);
+
+	setGraph(); //создание графа
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(WinW, WinH);
+	glutCreateWindow("window");
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouseClick);
+	glutPassiveMotionFunc(mouseMove);
+	glutMainLoop();
+}
