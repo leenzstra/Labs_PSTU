5c10fcc23e3c0f7b44f80b5cf14301383568a277
Ushakov Andrey РИС-19-1б <65588137+leenzstra@users.noreply.github.com>
2020-05-19 13:14:16 +0500

    Add files via upload

---
+++ 33_«Разработка калькулятора с использованием Windows Forms»_Вариант_6/SplashScreen.h
@@ -0,0 +1,168 @@
+#pragma once
+#include <Windows.h>
+
+namespace Calc 
+{
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class SplashScreen : public System::Windows::Forms::Form
+	{
+	public:
+		SplashScreen(void)
+		{
+			InitializeComponent();
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~SplashScreen()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	protected:
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::Timer^ timer1;
+
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+	private: System::ComponentModel::IContainer^ components;
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(SplashScreen::typeid));
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// panel2
+			// 
+			this->panel2->BackColor = System::Drawing::Color::Transparent;
+			this->panel2->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"panel2.BackgroundImage")));
+			this->panel2->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->panel2->Location = System::Drawing::Point(17, 360);
+			this->panel2->Margin = System::Windows::Forms::Padding(2);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(1000, 13);
+			this->panel2->TabIndex = 1;
+			// 
+			// timer1
+			// 
+			this->timer1->Enabled = true;
+			this->timer1->Interval = 15;
+			this->timer1->Tick += gcnew System::EventHandler(this, &SplashScreen::timer1_Tick);
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(39)), static_cast<System::Int32>(static_cast<System::Byte>(41)),
+				static_cast<System::Int32>(static_cast<System::Byte>(56)));
+			this->panel1->Controls->Add(this->pictureBox2);
+			this->panel1->Controls->Add(this->panel2);
+			this->panel1->Controls->Add(this->pictureBox1);
+			this->panel1->Location = System::Drawing::Point(72, 54);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(1037, 396);
+			this->panel1->TabIndex = 3;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.BackgroundImage")));
+			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->pictureBox1->Location = System::Drawing::Point(44, 38);
+			this->pictureBox1->Margin = System::Windows::Forms::Padding(2);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(283, 280);
+			this->pictureBox1->TabIndex = 2;
+			this->pictureBox1->TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox2->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.BackgroundImage")));
+			this->pictureBox2->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->pictureBox2->Location = System::Drawing::Point(380, 67);
+			this->pictureBox2->Margin = System::Windows::Forms::Padding(2);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(589, 266);
+			this->pictureBox2->TabIndex = 3;
+			this->pictureBox2->TabStop = false;
+			// 
+			// SplashScreen
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::Green;
+			this->ClientSize = System::Drawing::Size(1200, 731);
+			this->Controls->Add(this->panel1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Margin = System::Windows::Forms::Padding(2);
+			this->Name = L"SplashScreen";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"MyForm";
+			this->TransparencyKey = System::Drawing::Color::Green;
+			this->Load += gcnew System::EventHandler(this, &SplashScreen::SplashScreen_Load);
+			this->panel1->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+		Color SecondaryDark = Color::FromArgb(53, 60, 71);	//blackier
+		Color PrimaryDark = Color::FromArgb(62, 69, 83);	//not much black
+		Color SecondaryLight = Color::FromArgb(255, 255, 255);	//light
+		Color ThemeColor = Color::FromArgb(126, 20, 255);	//blue
+		Color PrimaryLight = Color::FromArgb(246, 250, 253); //whiter
+	
+	private: System::Void SplashScreen_Load(System::Object^ sender, System::EventArgs^ e)
+	{
+		this->panel1->BackColor = SecondaryDark;
+		panel2->Width = 0;
+	}
+	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) 
+	{
+		panel2->Width += 10;
+		if (panel2->Width >= 1000)
+		{
+			timer1->Stop();
+			Sleep(250);
+			this->Close();
+		}
+	}
+};
+}
